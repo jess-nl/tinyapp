0f247528e439be7dd8d4450990c7967a5ef5a64e
Jess <jnoriegaless@gmail.com>
2019-07-08 20:48:57 +0000

    1. Direct to a page that says 'Ok' after submitting a URL. 2. Generate 6 random characters in a function for later use

--- express_server.js
+++ express_server.js
@@ -1,13 +1,30 @@
+const bodyParser = require("body-parser");
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+function generateRandomString() {
+  let randomCharacters = '';
+  let char = '0123456789abcdefghijklmnopqrstuvwxyz';
+  
+  for (let i = 0; i < 6; i++) {
+    randomCharacters += char.charAt(Math.floor(Math.random() * char.length));
+  }
+  return randomCharacters;
+}
+generateRandomString();
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -24,15 +41,23 @@ app.get("/hello", (req, res) => {
 app.get("/set", (req, res) => {
   const a = 1;
   res.send(`a = ${a}`);
- });
-
+});
   
- app.get("/fetch", (req, res) => {
+app.get("/fetch", (req, res) => {
   const a = 1;
   res.send(`a = ${a}`);
- });
+});
 
- app.get("/urls", (req, res) => {
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
+app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
@@ -40,8 +65,4 @@ app.get("/set", (req, res) => {
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL };
   res.render("urls_show", templateVars);
-});
-
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
 });
