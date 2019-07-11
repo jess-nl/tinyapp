e900d3b515d7d7d7815021eea653d979dddb27b9
Jess <jnoriegaless@gmail.com>
2019-07-11 18:01:11 +0000

    User registration, login page with cookie/logout features.

--- express_server.js
+++ express_server.js
@@ -13,14 +13,36 @@ app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
+// ====================================================
+// ==================== Databases =====================
+// ====================================================
+
 // Database for the creation of all URLs
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+// Store our users
+const users = { 
+  "user1RandomID": {
+    id: "user1RandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+ "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+};
+
+// ====================================================
+// ================ Global functions ==================
+// ====================================================
+
 // Generate random characters for short URL
-function generateRandomString() {
+const generateRandomString = function() {
   let randomCharacters = '';
   let char = '0123456789abcdefghijklmnopqrstuvwxyz';
   
@@ -28,7 +50,11 @@ function generateRandomString() {
     randomCharacters += char.charAt(Math.random() * char.length);
   }
   return randomCharacters;
-}
+};
+
+// ====================================================
+// ===================== Routing ======================
+// ====================================================
 
 // Landing on index page, prints "Hello"
 app.get("/", (req, res) => {
@@ -47,14 +73,15 @@ app.get("/hello", (req, res) => {
 
 // Landing on /urls/new, displays template of urls_new.ejs
 app.get("/urls/new", (req, res) => {
-  let templateVars = { username: req.cookies["username"] };
+  let templateVars = { user: users[req.cookies["user_id"]] };
   res.render("urls_new", templateVars);
 });
 
-// Generate short URL based on the function generateRandomString(), then redirect.
+// Generate short URL based on generateRandomString() function, then redirect.
 app.post("/urls", (req, res) => {
   // Log the POST request body to the console
   console.log(req.body);
+
   let shortUrl = generateRandomString();
   urlDatabase[shortUrl] = req.body.longURL;
   console.log("urlDatabase:", urlDatabase);
@@ -63,16 +90,16 @@ app.post("/urls", (req, res) => {
   res.redirect(`/urls/${shortUrl}`);
 });
 
-// Landing on /urls, displays username at the very top & a list of short URLS in a column
-// Uses the urls_index.ejs template
+// Landing on /urls
 app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase, username: req.cookies["username"] };
+  
+  let templateVars = { urls: urlDatabase, user: users[req.cookies["user_id"]]};
   res.render("urls_index", templateVars);
 });
 
 // Directs to page and displays a short URL link
 app.get("/urls/:shortURL", (req, res) => {
-  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], username: req.cookies["username"] };
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], user: users[req.cookies["user_id"]] };
   res.render("urls_show", templateVars);
 });
 
@@ -102,19 +129,102 @@ app.post('/urls/:id', (req, res) => {
   res.redirect('/urls');
 });
 
-// Login route
-app.post("/login", (req, res) => {
-  const { id } = req.body.username;
-  console.log("req.body from login: ", req.body.username);
+// ===================== Register =====================
 
-  res.cookie('username', req.body.username);
-  res.redirect('/urls');
+// Registration landing
+app.get('/registration', function (req, res) {
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], user: users[req.cookies["user_id"]] };
+  res.render("urls_registration", templateVars);
+});
+
+// Add new user's email & password in database
+app.post("/registration", (req, res) => {
+  let email = req.body.email;
+  let password = req.body.password;
+
+  console.log("User's email:", req.body.email);
+  console.log("User's password:", req.body.password);
+
+  if (email === "" || password === "") {
+    res.status(403).end();
+  } else {
+    // Looking through each users
+    for (const user in users) {
+
+      // Check if current email is already taken
+      if (email === users[user].email) {
+        console.log("Email already taken");
+        res.status(403).end();
+      }
+    }
+
+    // Log new user
+    console.log("Created new user!");
+
+    // Generate unique user ID & add it to user database
+    let newId = `user${generateRandomString()}RandomID`;
+    const newUser = { id: newId, email, password };
+    users[newId] = newUser;
+
+    // Generate cookie
+    res.cookie('user_id', newId);
+
+    // Redirect
+    res.redirect('/urls');
+  }
+
+  // Log user database
+  console.log("User database:", users);
+});
+
+// ===================== Login =====================
+
+// Login landing
+app.get('/login', function (req, res) {
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], user: users[req.cookies["user_id"]] };
+  res.render("urls_login", templateVars);
+});
+
+
+// Login existing user with email & password
+app.post("/login", (req, res) => {
+  // Log the POST request body to the console
+  let email = req.body.email;
+  let password = req.body.password;
+  console.log("User's email:", req.body.email);
+  console.log("User's password:", req.body.password);
+
+  // If email and password inputs are left blank
+  if (email === "" || password === "") {
+    // Assign to error page
+    res.status(403).end();
+  }
+
+  let currentUser;
+  for (const key in users) {
+    // If email already exists
+    if (email === users[key].email) {
+      currentUser = users[key];
+      // If password already exists
+      if (password === currentUser.password) {
+        // Generate cookie
+        res.cookie('user_id', currentUser.id);
+        // Redirect to main page
+        res.redirect('/urls');
+      } else {
+        console.log("Email and password not in system, must register!");
+        // Assign to error page
+        res.status(403).end();
+      }
+    }
+  }
+  console.log('Wrong email or password! Must submit the right information or register!');
+  // Assign to error page
+  res.status(403).end();
 });
 
 // Logout route
 app.post("/logout", (req, res) => {
-  // const { id } = req.body.username;
-
-  res.clearCookie('username', req.body.username)
+  res.clearCookie('user_id');
   res.redirect('/urls');
 });
